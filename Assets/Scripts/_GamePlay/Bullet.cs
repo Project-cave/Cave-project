77d2a328ed4d1c8bc1e022a86953e14dc21b1ec2
wnvlf <139468663+wnvlf@users.noreply.github.com>
2026-03-14 22:00:58 +0900

    feat: 폴더 정리, 스킬 데이터 수정, 씬 연결 개발

---
+++ Assets/Scripts/_GamePlay/Bullet.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public enum BulletType { Normal };
+    public BulletType type;
+    public float Damage;
+    public int per;
+    public int id;
+
+    public float BulletTime;
+
+    Rigidbody2D rigid;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BulletTime += Time.deltaTime;
+
+        if(BulletTime > 5 && per != -1)
+        {
+            rigid.linearVelocity = Vector2.zero;
+            gameObject.SetActive(false);
+            BulletTime = 0;
+        }
+    }
+
+    public void Init(int id, float Damage, int per, Vector3 dir, int bulletVelocity)
+    {
+        this.Damage = Damage;
+        this.per = per;
+        this.id = id;      
+        rigid.linearVelocity = dir * bulletVelocity;        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!(collision.CompareTag("Enemy") || collision.CompareTag("Wall")) || per == -1)
+            return;
+        
+
+        per--;
+        
+
+        if(per == -1 || collision.CompareTag("Wall"))
+        {
+            rigid.linearVelocity = Vector2.zero;
+            gameObject.SetActive(false);
+        }
+
+        // Enemy에게 데미지 주는 로직
+        if (collision.CompareTag("Enemy"))
+        {
+            StatHandler stat = collision.GetComponent<StatHandler>();
+
+            if (stat != null)
+            {
+                stat.TakeDamage((int)Damage);
+            }
+
+            rigid.linearVelocity = Vector2.zero;
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/_System/PoolManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    public GameObject[] prefebs;
+    public List<GameObject>[] pools;
+
+    void Start()
+    {
+        pools = new List<GameObject>[prefebs.Length];
+        for (int index = 0; index < pools.Length; index++)
+        {
+            pools[index] = new List<GameObject>();
+        }
+    }
+
+    public GameObject Get(int index)
+    {
+        GameObject select = null;
+        foreach (GameObject item in pools[index])
+        {
+            if (!item.activeSelf)
+            {
+                select = item;
+                select.SetActive(true);
+                return select;
+            }
+        }
+
+        if (!select)
+        {
+            select = Instantiate(prefebs[index], transform);
+            pools[index].Add(select);
+        }
+
+        GameManager.instance.SpawnUnit(select);
+        return select;
+    }
+
+    // 임시 코드
+    public void EnemySpawn()
+    {
+        GameObject spawned = Get(1);
+        if (spawned != null && AudioManager.instance != null)
+        {
+            AudioManager.instance.PlaySfx(0);
+        }
+    }
+}
