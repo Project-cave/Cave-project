915706fe394e664caacf29661d9c3bc2aa7a88f4
chuwntjd <77573247+chuwntjd@users.noreply.github.com>
2026-03-24 18:19:16 +0900

    arrangement

--- Assets/Scripts/_System/UnitSpawner.cs
+++ Assets/Scripts/_System/UnitSpawner.cs
@@ -7,8 +7,13 @@ public class UnitSpawner : MonoBehaviour
 
     public void SpawnSword()
     {
-        GameObject select = GameManager.instance.pool.Get(0);   
-        select.GetComponent<Unit>().InitUnit(unitData[0]);
+        GameObject select = GameManager.instance.pool.Get(0);
+        UnitStatHandler statHandler = select.GetComponent<UnitStatHandler>();
+        if (statHandler != null)
+        {
+            statHandler.unitData = unitData[0];
+        }
+        select.GetComponent<Unit>().InitUnit();
         if (select != null && AudioManager.instance != null)
         {
             AudioManager.instance.PlaySfx(0);
@@ -19,7 +24,12 @@ public class UnitSpawner : MonoBehaviour
     public void SpawnWizard()
     {
         GameObject select = GameManager.instance.pool.Get(0);
-        select.GetComponent<Unit>().InitUnit(unitData[1]);
+        UnitStatHandler statHandler = select.GetComponent<UnitStatHandler>();
+        if (statHandler != null)
+        {
+            statHandler.unitData = unitData[1];
+        }
+        select.GetComponent<Unit>().InitUnit();
         if (select != null && AudioManager.instance != null)
         {
             AudioManager.instance.PlaySfx(0);
